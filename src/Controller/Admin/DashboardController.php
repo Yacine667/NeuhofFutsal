783a0b47c12d841136c19ee3e85d7d6e8d357ab6
Yacine667 <sid.yacinee@gmail.com>
2023-05-15 16:26:38 +0200

    ajout condition isgranted page admin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,26 +2,35 @@
 
 namespace App\Controller\Admin;
 
-use App\Entity\Actualite;
-use App\Entity\Entraineur;
+use App\Entity\Stade;
+use App\Entity\Ville;
 use App\Entity\Equipe;
 use App\Entity\Joueur;
 use App\Entity\Oppose;
+use App\Entity\Actualite;
 use App\Entity\Rencontre;
-use App\Entity\Stade;
-use App\Entity\Ville;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
-use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
-use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use App\Entity\Entraineur;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 
+
+    
 class DashboardController extends AbstractDashboardController
 {
-    #[Route('/admin', name: 'admin')]
+
+/**
+ * @Route("/admin", name="admin")
+ *
+ * @IsGranted("ROLE_ADMIN")
+ */
     public function index(): Response
     {
+        
         $routeBuilder = $this->container->get(AdminUrlGenerator::class);
         $url = $routeBuilder->setController(ActualiteCrudController::class)->generateUrl();
 
