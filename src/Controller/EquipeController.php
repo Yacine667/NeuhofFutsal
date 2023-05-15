20686b579b7b5b8ce9179098dff1515be89e71ac
Yacine667 <sid.yacinee@gmail.com>
2023-05-15 11:57:39 +0200

    avancement Css + ajout vue index equipe

--- src/Controller/EquipeController.php
+++ src/Controller/EquipeController.php
@@ -20,12 +20,30 @@ class EquipeController extends AbstractController
         ]);
     }
 
+    #[Route('/generalView', name: 'app_indexTeam')]
+    public function indexTeam(): Response
+    {
+        
+        return $this->render('generalView/teamView.html.twig', [
+            
+        ]);
+    }
+
     #[Route('/equipe2', name: 'app_equipe2')]
     public function index2(ManagerRegistry $doctrine, Equipe $equipe = null, Request $request): Response
     {
         $equipe = $doctrine->getRepository(Equipe::class)->findOneBy(['id' => 2 ] ,[]);
         return $this->render('equipe/index.html.twig', [
             'equipe' => $equipe,
+        ]);
+    }
+
+    #[Route('/generalView2', name: 'app_indexTeam2')]
+    public function indexTeam2(): Response
+    {
+        
+        return $this->render('generalView/teamView.html.twig', [
+            
         ]);
     }
 }
