d421a121efaf0ce1b0eaa7a5f6b5a1b3ba13445d
Yacine667 <sid.yacinee@gmail.com>
2023-05-21 21:16:29 +0200

    rectif easyadmin comments

--- src/Controller/Admin/PostCrudController.php
+++ src/Controller/Admin/PostCrudController.php
@@ -4,8 +4,12 @@ namespace App\Controller\Admin;
 
 use App\Entity\Post;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class PostCrudController extends AbstractCrudController
@@ -21,5 +25,17 @@ class PostCrudController extends AbstractCrudController
 
         ->remove(Crud::PAGE_INDEX, Action::NEW)
         ->remove(Crud::PAGE_INDEX, Action::EDIT);
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+
+        yield IdField::new('id')
+            ->onlyOnIndex();
+        yield Field::new('texte_post');
+        yield AssociationField::new('user');
+        yield DateField::new('date_creation');
+        
+
     }
 }
